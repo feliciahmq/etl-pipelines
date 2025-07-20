ad3df84c51fc07c44937dcb2b3f0757759297287
Felicia Hwang <131253731+feliciahmq@users.noreply.github.com>
2025-07-20 21:40:16 +0800

    debug

--- part2/streamlit_app.py
+++ part2/streamlit_app.py
@@ -18,6 +18,9 @@ if tab == "📊 Local Sales":
 
     st.subheader("Monthly Sales by Category")
     df_monthly_sales = load_data("part2/outputs/monthly_sales_by_category")
+    st.write("File preview:")
+    st.write(load_data("outputs/monthly_sales_by_category").head())
+    st.write("Columns:", df_monthly_sales.columns)
     df_monthly_sales["Month"] = pd.to_datetime(df_monthly_sales["Month"], format="%Y-%m")
 
     st.altair_chart(
